38de4422ebcaccf5931586958f9c7a7eb2846069
Daman <singhdaman4321@gmail.com>
2017-10-14 17:12:49 +0530

    Initial commit

---
+++ app/src/main/java/com/singh/daman/gentletoast/MainActivity.java
@@ -0,0 +1,13 @@
+package com.singh.daman.gentletoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
